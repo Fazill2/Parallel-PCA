733eb508252ae7537e868a1ba304847a4afcfb61
Jakub Torłop <kubatorlop@gmail.com>
2024-05-29 00:16:24 +0200

    semi working cuda

--- cudaPca.cu
+++ cudaPca.cu
@@ -12,61 +12,73 @@
 #include <iomanip>
 #include <cuda_runtime.h>
 #include <cublas_v2.h>
+#include <cuda_runtime.h>
+#include <cusolverDn.h>
 
 using namespace std;
- 
-vector<vector<double>> cudaMatrixMultiply(double *A, double *B, double *C, int M, int N, int K){
-    cudaError_t cudaStat;
-    cublasStatus_t stat;
-    cublasHandle_t handle;
 
-    double *h_A, *h_B, *h_C;
-    double *d_A, *d_B, *d_C;
-    const double alpha = 1.0;
-    const double beta = 0.0;
-
-    h_A = (double *)malloc(M * N * sizeof(double));
-    h_B = (double *)malloc(N * K * sizeof(double));
-    h_C = (double *)malloc(M * K * sizeof(double));
-
-    for (int i = 0; i < M * N; ++i) {
-        h_A[i] = static_cast<double>(i);
+vector<double> flatten(const vector<vector<double>>& matrix) {
+    vector<double> flattened;
+    for (const auto& row : matrix) {
+        flattened.insert(flattened.end(), row.begin(), row.end());
     }
-
-    for (int i = 0; i < N * K; ++i) {
-        h_B[i] = static_cast<double>(i);
-    }
-
-    cudaStat = cudaMalloc((void **)&d_A, M * N * sizeof(double));
-    cudaStat = cudaMalloc((void **)&d_B, N * K * sizeof(double));
-    cudaStat = cudaMalloc((void **)&d_C, M * K * sizeof(double));
-
-    stat = cublasCreate(&handle);
-
-    stat = cublasSetMatrix(M, N, sizeof(double), h_A, M, d_A, M);
-    stat = cublasSetMatrix(N, K, sizeof(double), h_B, N, d_B, N);
-
-    stat = cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, M, K, N, &alpha, d_A, M, d_B, N, &beta, d_C, M);
-
-    stat = cublasGetMatrix(M, K, sizeof(double), d_C, M, h_C, M);
-
-    // Cleanup
-    vector<vector<double>> C_vector(M, vector<double>(K, 0.0));
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < K; j++) {
-            C_vector[i][j] = h_C[i * K + j];
-        }
-    }
-    
-    cudaFree(d_A);
-    cudaFree(d_B);
-    cudaFree(d_C);
-    cublasDestroy(handle);
-    free(h_A);
-    free(h_B);
-    free(h_C);
-    return C_vector;
+    return flattened;
 }
+
+vector<double> calculateEigenvaluesCuda(const vector<vector<double>>& covMatrix){
+    vector<double> flattenedMatrix = flatten(covMatrix);
+
+    // Dimension of the covariance matrix
+    int N = covMatrix.size(); // Assuming covMatrix is square
+
+    // CUDA variables
+    cusolverDnHandle_t handle;
+    cusolverDnCreate(&handle);
+
+    // Workspace and devInfo variables
+    int *devInfo;
+    cudaMalloc(&devInfo, sizeof(int));
+
+    // Perform eigenvalue decomposition
+    double *d_A;
+    cudaMalloc(&d_A, N * N * sizeof(double));
+    cudaMemcpy(d_A, flattenedMatrix.data(), N * N * sizeof(double), cudaMemcpyHostToDevice);
+
+    // Allocate space for eigenvalues
+    double *d_W;
+    cudaMalloc(&d_W, N * sizeof(double));
+
+    // // Allocate space for eigenvectors
+    // double *d_V;
+    // cudaMalloc(&d_V, N * N * sizeof(double));
+
+    // Determine workspace size
+    int work_size;
+    cusolverDnDsyevd_bufferSize(handle, CUSOLVER_EIG_MODE_VECTOR, CUBLAS_FILL_MODE_UPPER, N, d_A, N, d_W, &work_size);
+
+    // Allocate workspace
+    double *work;
+    cudaMalloc(&work, work_size);
+
+    // Perform eigenvalue decomposition
+    cusolverDnDsyevd(handle, CUSOLVER_EIG_MODE_VECTOR, CUBLAS_FILL_MODE_UPPER, N, d_A, N, d_W, work, work_size, devInfo);
+
+    // Copy eigenvalues back to host
+    vector<double> eigenvalues(N);
+    cudaMemcpy(eigenvalues.data(), d_W, N * sizeof(double), cudaMemcpyDeviceToHost);
+
+    // Print eigenvalues
+    
+    // Clean up
+    cudaFree(devInfo);
+    cudaFree(d_A);
+    cudaFree(d_W);
+    cudaFree(work);
+    cusolverDnDestroy(handle);
+    
+    return eigenvalues;
+}
+
 
 // Function to read data from a file
 vector<vector<double>> readData(string filename) {
@@ -85,6 +97,47 @@ vector<vector<double>> readData(string filename) {
     return data;
 }
 
+void generateRandomData(int rows, int columns, string filename) {   
+    ofstream file;
+    vector<vector<double>> data(columns, vector<double>(rows));
+    
+    for (int i = 0; i < columns; i++) {
+        for (int j = 0; j < rows; j++) {
+            data[i][j] = (double)rand() / RAND_MAX;
+        }
+    }
+    // for random number of random columns generate random linear equation ax+b where x is another random column
+    int randomColumns = rand() % columns;
+    vector<double> a(randomColumns);
+    vector<double> b(randomColumns);
+    for (int i = 0; i < randomColumns; i++) {
+        a[i] = (double)rand() / RAND_MAX;
+        b[i] = (double)rand() / RAND_MAX;
+    }
+    // pca requires data to be somewhat dependent
+    for (int i = 0; i < randomColumns; i++) {
+        int randColumn1 = rand() % columns;
+        int randColumn2 = rand() % columns;
+        for (int j = 0; j < rows; j++) {
+            data[randColumn1][j] = a[i] * data[randColumn2][j] + b[i];
+        }
+    }
+
+    file.open(filename);
+    for (int i = 0; i < data[0].size(); i++) {
+        for (int j = 0; j < data.size(); j++) {
+            file << data[j][i];
+            if (j != data.size() - 1) {
+                file << ",";
+            }
+        }
+        if (i != data[0].size() - 1) {
+            file << endl;
+        }
+    }
+    file.close();
+}
+
 void saveData(vector<vector<double>> data, string filename) {
     ofstream file;
     file.open(filename);
@@ -97,7 +150,7 @@ void saveData(vector<vector<double>> data, string filename) {
     file.close();
 }
 
-double** vectorMatrixToDouble(vector<vector<double>> data) {
+double** vectorMatrixToDouble(vector<vector<double>>& data) {
     int rows = data.size();
     int columns = data[0].size();
     double** matrix = new double*[rows];
@@ -160,7 +213,7 @@ vector<vector<double>> covarianceMatrix(vector<vector<double>> data) {
     return covMatrix;
 }
 // Helper function to transpose a matrix, complexity O(n*m), n - rows, m - columns
-vector<vector<double>> transposeMatrix(const vector<vector<double>> A) {
+vector<vector<double>> transposeMatrix(vector<vector<double>> A) {
     int n = A.size();
     int m = A[0].size();
     vector<vector<double>> AT(m, vector<double>(n, 0.0));
@@ -173,23 +226,23 @@ vector<vector<double>> transposeMatrix(const vector<vector<double>> A) {
 }
 
 // Helper function to perform matrix multiplication, complexity O(n*m*p), n - rows, m - columns, p - columns
-vector<vector<double>> matrixMultiply(const vector<vector<double>>& A, const vector<vector<double>>& B) {
+vector<vector<double>> matrixMultiply(vector<vector<double>>& A, vector<vector<double>>& B) {
     int n = A.size();
     int m = B.size();
     int p = B[0].size();
-    double** matrixA = vectorMatrixToDouble(A);
-    double** matrixB = vectorMatrixToDouble(B);
-    double** matrixC = new double*[n];
+    vector<vector<double>> C(n, vector<double>(p, 0.0));
     for (int i = 0; i < n; i++) {
-        matrixC[i] = new double[p];
+        for (int j = 0; j < p; j++) {
+            for (int k = 0; k < m; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
     }
-    vector<vector<double>> C = cudaMatrixMultiply(*matrixA, *matrixB, *matrixC, n, m, p);
-
     return C;
 }
 
 // Helper function to perform matrix subtraction, complexity O(n*m), n - rows, m - columns
-vector<vector<double>> matrixSubtract(const vector<vector<double>>& A, const vector<vector<double>>& B) {
+vector<vector<double>> matrixSubtract(vector<vector<double>>& A, vector<vector<double>>& B) {
     int n = A.size();
     int m = A[0].size();
     vector<vector<double>> C(n, vector<double>(m, 0.0));
@@ -202,7 +255,7 @@ vector<vector<double>> matrixSubtract(const vector<vector<double>>& A, const vec
 };
 
 // Helper function to calculate the norm of a vector, complexity O(n)
-double norm(const vector<double>& x) {
+double norm(vector<double>& x) {
     double sum = 0.0;
     for (int i = 0; i < x.size(); ++i) {
         sum += x[i] * x[i];
@@ -211,7 +264,7 @@ double norm(const vector<double>& x) {
 }
 
 // Helper function to perform scalar multiplication, complexity O(n)
-vector<double> scalarMultiply(const vector<double>& x, double alpha) {
+vector<double> scalarMultiply(vector<double>& x, double alpha) {
     vector<double> result(x.size(), 0.0);
     for (int i = 0; i < x.size(); ++i) {
         result[i] = alpha * x[i];
@@ -229,7 +282,7 @@ vector<vector<double>> identityMatrix(int n) {
 }
 
 // hausolder transformation
-pair<vector<vector<double>>, vector<vector<double>>> householderTransformation(const vector<vector<double>>& A) {
+pair<vector<vector<double>>, vector<vector<double>>> householderTransformation(vector<vector<double>>& A) {
     int n = A.size();
     vector<vector<double>> Q = identityMatrix(n);
     vector<vector<double>> R = A;
@@ -240,26 +293,42 @@ pair<vector<vector<double>>, vector<vector<double>>> householderTransformation(c
             x[j - i] = R[j][i];
         }
 
-        double alpha = norm(x);
+        double alpha = (x[0] >= 0) ? -norm(x) : norm(x);
 
         vector<double> e(n - i, 0.0);
         e[0] = 1.0;
 
-        vector<double> v(n - i, 0.0);
+        vector<double> v(n - i);
         for (int j = 0; j < n - i; ++j) {
-            v[j] = x[j] - alpha * e[j];
+            v[j] = x[j] + alpha * e[j];
         }
-        vector<double> normVec(n - i, 0.0);
-        normVec = scalarMultiply(v, 1.0 / norm(v));
-        
-        vector<vector<double>> H = identityMatrix(n);
-        for (int j = 0; j < n - i; ++j) {
-            for (int k = 0; k < n - i; ++k) {
-                H[j + i][k + i] -= 2 * normVec[j] * normVec[k];
+
+        double vNorm = norm(v);
+        if (vNorm != 0) {
+            v = scalarMultiply(v, 1.0 / vNorm);
+        }
+
+        // Apply Householder transformation to R
+        for (int j = i; j < n; ++j) {
+            double dotProduct = 0.0;
+            for (int k = i; k < n; ++k) {
+                dotProduct += v[k - i] * R[k][j];
+            }
+            for (int k = i; k < n; ++k) {
+                R[k][j] -= 2.0 * v[k - i] * dotProduct;
             }
         }
-        Q = matrixMultiply(Q, H);
-        R = matrixMultiply(H, R);
+
+        // Apply Householder transformation to Q
+        for (int j = 0; j < n; ++j) {
+            double dotProduct = 0.0;
+            for (int k = i; k < n; ++k) {
+                dotProduct += v[k - i] * Q[j][k];
+            }
+            for (int k = i; k < n; ++k) {
+                Q[j][k] -= 2.0 * v[k - i] * dotProduct;
+            }
+        }
     }
     return make_pair(Q, R);
 }
@@ -269,29 +338,24 @@ vector<double> calculateEigenvalues(vector<vector<double>>& matrix) {
     int n = matrix.size();
     vector<vector<double>> ATemp = matrix;
     
-    vector<vector<double>> R = matrix;
+    const double epsilon = 1e-4;  // Convergence threshold
     bool converged = false;
-    for (;;) {
+    
+    while (!converged) {
         pair<vector<vector<double>>, vector<vector<double>>> QR = householderTransformation(ATemp);
-        R = QR.second;
         vector<vector<double>> new_ATemp = matrixMultiply(QR.second, QR.first);
+        
+        converged = true;
         for (int i = 0; i < n; ++i) {
-            bool flag = false;
             for (int j = 0; j < n; ++j) {
-                if (abs(new_ATemp[i][j] - ATemp[i][j]) > 1e-6) {
-                    flag = true;
+                if (abs(new_ATemp[i][j] - ATemp[i][j]) > epsilon) {
+                    converged = false;
                     break;
                 }
-                if (i == n - 1 && j == n - 1) {
-                    converged = true;
-                }
             }
-            if (flag) {
+            if (!converged) {
                 break;
             }
-        }
-        if (converged){
-            break;
         }
         ATemp = new_ATemp;
     }
@@ -302,25 +366,8 @@ vector<double> calculateEigenvalues(vector<vector<double>>& matrix) {
     return eigenvalues;
 }
 
-std::vector<double> backSubstitution(const std::vector<std::vector<double>>& matrix, const std::vector<double>& b) {
-    int n = matrix.size();
-    std::vector<double> x(n, 0.0);
-
-    for (int i = n - 1; i >= 0; --i) {
-        double sum = 0.0;
-        for (int j = i + 1; j < n; ++j) {
-            sum += matrix[i][j] * x[j];
-        }
-        x[i] = (b[i] - sum) / matrix[i][i];
-    }
-
-    return x;
-}
-
 vector<double> solveLinearEquations(vector<vector<double>> A, vector<double> b) {
     int n = A.size();
-
-    
     for (int i = 0; i < n; i++) {
         // Make the diagonal element non-zero
         if (A[i][i] == 0) {
@@ -361,7 +408,7 @@ vector<vector<double>> calculateEigenvectors(vector<vector<double>>& matrix, vec
         vector<double> b(n, 0.0);
         vector<double> x = solveLinearEquations(transposeMatrix(A), b);
         for (int j = 0; j < n; ++j) {
-            eigenvectors[j][i] = x[j];
+            eigenvectors[i][j] = x[j];
         }
     }
     return eigenvectors;
@@ -415,20 +462,26 @@ vector<vector<double>> pca(vector<vector<double>>& data) {
         standardizedData[i] = standardize(column);
     }
     vector<vector<double>> covMatrix = covarianceMatrix(standardizedData);
-    vector<double> eigenvalues = calculateEigenvalues(covMatrix);
+    vector<double> eigenvalues = calculateEigenvaluesCuda(covMatrix);
+
+    cout << "Eigenvalues: ";
+    for (int i = 0; i < eigenvalues.size(); i++) {
+        cout << eigenvalues[i] << " ";
+    }
     vector<vector<double>> eigenvectors = calculateEigenvectors(covMatrix, eigenvalues);
     pair<vector<double>, vector<vector<double>>> sortedEigenvaluesVectors = sortEigenvalues(eigenvalues, eigenvectors);
     vector<double> sortedEigenvalues = sortedEigenvaluesVectors.first;
     vector<vector<double>> sortedEigenvectors = sortedEigenvaluesVectors.second;
-    // cout << "Eigenvalues: ";
-    // for (int i = 0; i < sortedEigenvalues.size(); i++) {
-    //     cout << fixed << setprecision(5) << sortedEigenvalues[i] << " ";
-    // }
     vector<double> explainedVariance = calculateCumulativeSum(eigenvalues);
     int thresholdIndex = findThresholdIndex(explainedVariance, 0.8);
     vector<vector<double>> usefulComponents(thresholdIndex + 1);
     for (int i = 0; i < thresholdIndex + 1; i++) {
-        usefulComponents[i] = eigenvectors[i];
+        // normalize the eigenvectors
+        double norm = sqrt(inner_product(sortedEigenvectors[i].begin(), sortedEigenvectors[i].end(), sortedEigenvectors[i].begin(), 0.0));
+        for (int j = 0; j < sortedEigenvectors[i].size(); j++) {
+            sortedEigenvectors[i][j] /= norm;
+        }
+        usefulComponents[i] = sortedEigenvectors[i];
     }
     return matrixMultiply(usefulComponents, standardizedData);
 }
@@ -436,20 +489,36 @@ vector<vector<double>> pca(vector<vector<double>>& data) {
 
 
 int main(){
-    // create array of strings for file names
-    string fileNames[] = {"randomData/data_0.csv", "randomData/data_1.csv", "randomData/data_2.csv", "randomData/data_3.csv", 
-            "randomData/data_4.csv", "randomData/data_5.csv", "randomData/data_6.csv", "randomData/data_7.csv"};
-    // string fileNames[] = {"data/data.csv"};
-    
-    for (int i = 0; i < 8; i++) {
+     string fileNames[] = {"randomData/data_0.csv", "randomData/data_1.csv", "randomData/data_2.csv", "randomData/data_3.csv", 
+        "randomData/data_4.csv", "randomData/data_5.csv", "randomData/data_6.csv", "randomData/data_7.csv", 
+        "randomData/data_8.csv", "randomData/data_9.csv", "randomData/data_10.csv", "randomData/data_11.csv"};
+    int rows = 1000;
+    int columns[] = {10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54};
+    float times[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    // for (int i = 0; i < 12; i++) {
+    //     generateRandomData(rows, columns[i], fileNames[i]);
+    // }
+    for (int i = 0; i < 12; i++) {
         vector<vector<double>> data = readData(fileNames[i]);
         auto start_time = chrono::high_resolution_clock::now();
         vector<vector<double>> pcaData = pca(data);
         auto end_time = chrono::high_resolution_clock::now();
         auto time = end_time - start_time;
+        int timeTaken = chrono::duration_cast<chrono::milliseconds>(time).count();
         cout << "Time taken: " << time/chrono::milliseconds(1) << " ms" << endl;
+        times[i] =  (float)timeTaken / 1000.0;
         saveData(pcaData, "./output/output_" + to_string(i + 1) + ".csv");
     }
-
+    // save times to file for plotting
+    ofstream file;
+    file.open("cudaTimes.txt");
+    file << "[";
+    for (int i = 0; i < 12; i++) {
+        file << times[i];
+        if (i != 11) {
+            file << ",";
+        }
+    }
+    file << "]";
     return 0;
 }
